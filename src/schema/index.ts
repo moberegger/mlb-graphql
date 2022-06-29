b6e60609d78099f74b279e7557f7f7a452229d05
Michael Oberegger <moberegger@hotmail.com>
2022-06-29 19:25:06 -0400

    Add positions to schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,6 +3,6 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 
 export default async () =>
   makeExecutableSchema({
-    typeDefs: await loadFiles("src/**/*.graphql"),
-    resolvers: await loadFiles("src/**/resolvers.(t|j)s"),
+    typeDefs: await loadFiles("./**/*.graphql"),
+    resolvers: await loadFiles("./**/resolvers.(t|j)s"),
   });
