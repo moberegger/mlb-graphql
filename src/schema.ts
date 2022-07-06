bbf99d2679dce288c0430d1388e5a4c0d8778c3e
Michael Oberegger <moberegger@hotmail.com>
2022-07-05 21:51:14 -0400

    Added Player integration tests

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,9 @@
 import { loadFiles } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
-export default makeExecutableSchema({
-  typeDefs: await loadFiles("./**/*.graphql"),
-  resolvers: await loadFiles("./**/resolvers.(t|j)s"),
-});
+const [typeDefs, resolvers] = await Promise.all([
+  loadFiles("./**/*.graphql"),
+  loadFiles("./**/resolvers.ts"),
+]);
+
+export default makeExecutableSchema({ typeDefs, resolvers });
---
+++ src/schema/types/Player/Player.test.ts
@@ -0,0 +1,70 @@
+import { gql } from "apollo-server";
+import nock from "nock";
+
+import server from "../../../server.js";
+
+nock.disableNetConnect();
+
+describe("Player type", () => {
+  describe("on Query", () => {
+    it("returns a player", async () => {
+      const id = "abc";
+      nock("https://api.sportradar.us")
+        .get(`/mlb/trial/v7/en/players/${id}/profile.json`)
+        .query({ api_key: "" })
+        .reply(200, {
+          player: {
+            id,
+            position: "P",
+            primary_position: "SP",
+            full_name: "Jon Lester",
+          },
+        });
+
+      const { data, errors } = await server.executeOperation({
+        query: gql`
+          query ($id: ID!) {
+            player(id: $id) {
+              id
+              name
+              position
+              primaryPosition
+            }
+          }
+        `,
+        variables: { id },
+      });
+
+      expect(errors).toBeUndefined();
+      expect(data?.["player"]).toEqual({
+        id,
+        position: "PITCHER",
+        primaryPosition: "STARTING_PITCHER",
+        name: "Jon Lester",
+      });
+    });
+
+    it("returns null", async () => {
+      const id = "abc";
+      nock("https://api.sportradar.us")
+        .get(`/mlb/trial/v7/en/players/${id}/profile.json`)
+        .query({ api_key: "" })
+        .reply(404);
+
+      const { data, errors } = await server.executeOperation({
+        query: gql`
+          query ($id: ID!) {
+            player(id: $id) {
+              id
+              name
+            }
+          }
+        `,
+        variables: { id },
+      });
+
+      expect(errors).toBeUndefined();
+      expect(data?.["player"]).toBeNull();
+    });
+  });
+});
