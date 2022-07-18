a6aff06c2b632a7334a81edf7ad9781549172612
Michael Oberegger <moberegger@hotmail.com>
2022-07-18 18:32:07 -0400

    Setup server side caching

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginCacheControl } from "apollo-server-core";
+import cache from "apollo-server-plugin-response-cache";
 
 import emitter from "./core/emitter.js";
 import schema from "./schema.js";
 import context from "./context.js";
+
+// XXX: Hack to get the default export working with ESM
+// See: https://github.com/apollographql/apollo-server/issues/6022
+const ApolloServerPluginResponseCache = (cache as any).default as typeof cache;
 
 export default new ApolloServer({
   schema,
@@ -13,6 +18,7 @@ export default new ApolloServer({
   allowBatchedHttpRequests: false,
   plugins: [
     ApolloServerPluginCacheControl({ defaultMaxAge: 300 }),
+    ApolloServerPluginResponseCache(),
     {
       serverWillStart: async () => ({
         async serverWillStop() {
