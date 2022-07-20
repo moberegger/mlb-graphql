6e4d96dbb796fabc923d7d338322f947bf7a6368
Michael Oberegger <moberegger@hotmail.com>
2022-07-20 14:43:42 -0400

    Allow seasons to be filtered

--- src/schema/types/Season.resolvers.ts
+++ src/schema/types/Season.resolvers.ts
@@ -1,13 +1,24 @@
 import type { ConnectionArguments } from "graphql-relay";
 import type { Context } from "../../context.js";
-import type { Player, Season } from "../../datasources/SportRadarAPI.js";
+import type {
+  Player,
+  Season,
+  SeasonType,
+} from "../../datasources/SportRadarAPI.js";
 import makeConnection from "../helpers/makeConnection.js";
 
 export default {
   Player: {
-    seasons: makeConnection<Player, Context, ConnectionArguments, Season>()(
-      (player: Player, _: ConnectionArguments, ctx: Context) =>
-        ctx.services.season.findAllByPlayerId(player.id)
+    seasons: makeConnection<
+      Player,
+      Context,
+      ConnectionArguments & { filterBy: SeasonType | "ALL" },
+      Season
+    >()(
+      (player: Player, args: { filterBy: SeasonType | "ALL" }, ctx: Context) =>
+        ctx.services.season.findAllByPlayerId(player.id, {
+          filterBy: { type: args.filterBy },
+        })
     ),
   },
 };
